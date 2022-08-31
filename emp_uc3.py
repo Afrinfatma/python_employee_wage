78257bb13d8ccec9f89a8b2263ed18ead81c3fd8
Afrinfatma <afrinfatma4@gmail.com>
2022-09-01 00:40:30 +0530

    Refactored: emp wage Using class methods

--- emp_uc3.py
+++ emp_uc3.py
@@ -1,39 +1,38 @@
 import random
-class Employee_wage:
 
-    def calculate_emp_wage(self):
-        part_time = 1
-        full_time = 2
-        emp_rate_per_hr = 20
-        num_of_working_days=20
+
+class Employeewage:
+    def __init__(self):
+        self.part_time = 1
+        self.full_time = 2
+
+    def compute_emp_wage(self, company, emp_rate_per_hr, num_of_working_days, max_hrs_per_month):
         total_emp_wage = 0
-        emp_hrs=0
-        total_working_days=0
-        total_working_hrs=0
-        max_hrs_in_month=100
-        for day in range (num_of_working_days):
-            total_working_days=total_working_days+1
-            for hrs in range (max_hrs_in_month):
-                emp_check = random.randint(0, 2)
-                if emp_check == 1:
-                    print("Employee is present as part time")
-                    emp_hrs= 4
+        emp_hrs = 0
+        total_working_days = 0
+        total_working_hrs = 0
 
-                elif emp_check == 2:
-                    print("Employee is present as full time")
-                    emp_hrs= 8
+        while total_working_hrs <= max_hrs_per_month and total_working_days <= num_of_working_days:
+            total_working_days = total_working_days + 1
+            emp_check = random.randint(0, 2)
 
-                else:
-                        print("Employee is absent")
-                total_working_hrs =total_working_hrs + emp_hrs
-                print('day ', total_working_days, "emp hours", emp_hrs)
-            total_emp_wage=emp_rate_per_hr * total_working_hrs
+            if emp_check == 1:
+                print("Employee is present as part time")
+                emp_hrs = 4
+
+            elif emp_check == 2:
+                print("Employee is present as full time")
+                emp_hrs = 8
+
+            else:
+                print("Employee is absent")
+            total_working_hrs = total_working_hrs + emp_hrs
+            print('day ', total_working_days, "emp hours", emp_hrs)
+        total_emp_wage = emp_rate_per_hr * total_working_hrs
+
+        print("Total wage of an employee for a month", total_emp_wage)
 
 
-        print("Total wage of an employee for a month" ,total_emp_wage )
-
-
-if __name__=='__main__':
-    emp=Employee_wage()
-    emp.calculate_emp_wage()
-
+if __name__ == '__main__':
+    emp = Employeewage()
+    emp.compute_emp_wage("Reliance", 20, 20, 100)
